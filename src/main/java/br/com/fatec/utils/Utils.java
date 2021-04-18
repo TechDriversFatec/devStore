7140c8f7d5dcc1a31ce4b600d4023344dd97d2de
Junio Sendreto <junio.sendreto@hotmail.com>
2021-04-18 15:29:04 -0300

    Criação da função de LOG e retorno de data.

--- src/main/java/br/com/fatec/utils/Utils.java
+++ src/main/java/br/com/fatec/utils/Utils.java
@@ -1,6 +1,11 @@
 package br.com.fatec.utils;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Utils {
 
@@ -36,6 +41,50 @@ public class Utils {
 		if (!file.exists()) {
 			file.mkdirs();
 		}
+	}
+	
+	public static void escreverLog(String texto) {
+		CAMINHOAPLICACAO = System.getProperty("user.dir") + File.separator;
+		File file = new File(CAMINHOAPLICACAO + LOG + File.separator + retornaData(1));
+		
+		if(!file.exists()) {
+			file.mkdirs();
+		}
+		
+		FileWriter fileWriter;
+		BufferedWriter escrever;
+		try {
+			file = new File(CAMINHOAPLICACAO + LOG + File.separator + retornaData(1) + File.separator + "LOG.txt");
+			
+			if(file.exists()) {
+				fileWriter = new FileWriter(file, true);
+			}else {
+				fileWriter = new FileWriter(file);
+			}
+				
+			escrever = new BufferedWriter (fileWriter);
+	        escrever.append("[" + retornaData(2) + "] " + texto);
+	        escrever.newLine();
+	        escrever.close();
+	        fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static String retornaData(Integer tipo) {
+		Date data = new Date();
+		SimpleDateFormat formatar = new SimpleDateFormat();
+		
+	    if(tipo == 1) {
+	    	formatar = new SimpleDateFormat("ddMMyyyy");
+	    }else if(tipo == 2) {
+	    	formatar = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss" );
+	    }
+	    
+	    return formatar.format(data);
+		
 	}
 
 }
